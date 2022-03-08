0c6baa42e2f5b44ce1a02c8e7edc5af1961a2316
front-end-dev-ops-at-405670469700 <arindambhowal10@gmail.com>
2022-03-08 12:41:23 +0530

    initial commit

---
+++ home-page-contents/hero-cards-testimonials/index.js
@@ -0,0 +1,40 @@
+// HERO Animation
+document.addEventListener("mousemove", parallax);
+function parallax(e) {
+    this.querySelectorAll('.layer').forEach(layer => {
+        const speed = layer.getAttribute('data-speed')
+
+        const x = (window.innerWidth - e.pageX * speed) / 100
+        const y = (window.innerHeight - e.pageY * speed) / 100
+
+        layer.style.transform = `translateX(${x}px) translateY(${y}px)`
+    })
+}
+
+//---------------- SCROLL BUTTON------------
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+// join us button
+var $logo = $('#join');
+$(document).scroll(function() {
+    $logo.css({display: $(this).scrollTop() > 100? "block":"none"});
+});
